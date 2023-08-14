a51bc8932bb65e322a9794ccfc89ddd821a467f1
BirkasTivadar <birkastivadar@gmail.com>
2023-08-14 19:41:32 +0200

    Konfiguráció Java-ban mintapélda kész

--- employees/src/main/java/employees/HelloController.java
+++ employees/src/main/java/employees/HelloController.java
@@ -3,17 +3,31 @@ package employees;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.time.LocalDateTime;
+
+@RestController("/")
 public class HelloController {
 
     private final HelloService helloService;
+    private final EmployeesService employeesService;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, EmployeesService employeesService) {
         this.helloService = helloService;
+        this.employeesService = employeesService;
     }
 
-    @GetMapping("/")
-    public String sayHello() {
+    @GetMapping("controller")
+    public String sayHelloController() {
+        return "Hello Controller " + LocalDateTime.now();
+    }
+
+    @GetMapping("service")
+    public String sayHelloService() {
         return helloService.sayHello();
+    }
+
+    @GetMapping("employee")
+    public String sayHelloEmployee() {
+        return employeesService.sayHello();
     }
 }
